a9a3ec97e9d3c59de52bdbb51c7a705e005055be
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-03-29 12:00:25 -0500

    OK?

--- ArrayListUtilities.java
+++ ArrayListUtilities.java
@@ -2,6 +2,20 @@ import java.util.ArrayList;
 
 public class ArrayListUtilities {
     public static int getListMax(ArrayList<Integer> items) {
-        return 0;
+        int max = 0;
+        for(int n:items){
+            if(max<n){
+                max=n;
+            }
+        }
+        return max;
+    }
+    public static String withoutLongWords(ArrayList<String> words){ //This error needs to be returned
+        ArrayList<String> senceten;
+        for(String text:words){
+            if(text.length()<6){
+                senceten.add(text); // this needs to be changed
+            }
+        }
     }
 }
